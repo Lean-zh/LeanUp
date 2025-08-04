35049e9a4089ec20d871bb7592b6bdd43ba0f2aa
rex <1073853456@qq.com>
2025-08-05 00:57:24 +0800

    add import

--- leanup/__init__.py
+++ leanup/__init__.py
@@ -2,4 +2,12 @@
 
 __author__ = """Lean-zh Community"""
 __email__ = 'leanprover@outlook.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
+
+from .repo import RepoManager, ElanManager, LeanRepo
+from .utils import setup_logger, execute_command, working_directory
+
+__all__ = [
+    'RepoManager', 'ElanManager', 'LeanRepo',
+    "setup_logger", "execute_command", "working_directory"
+]
